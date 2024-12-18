aa98b0b78330fc76962c924129d88c67939529c2
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-12-18 15:33:17 -0300

    add initial input pages

---
+++ src/app/new/step-2/step-2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step-2',
+  imports: [],
+  templateUrl: './step-2.component.html',
+  styleUrl: './step-2.component.css'
+})
+export class Step2Component {
+
+}
---
+++ src/app/new/step-3/step-3.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step-3',
+  imports: [],
+  templateUrl: './step-3.component.html',
+  styleUrl: './step-3.component.css'
+})
+export class Step3Component {
+
+}
