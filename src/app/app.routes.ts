5c7317f5c44a510e41eb800e9986e8605901615f
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-12-18 15:32:56 -0300

    add initial routing system

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { WelcomePage } from './welcome/welcome.component';
+
+export const routes: Routes = [
+    { path: '', component: WelcomePage },
+];
