0570590b569ebf3241be5eb8065f5a26c6f07e52
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-12-18 15:55:14 -0300

    add endpoints

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 
 import { WelcomePage } from './welcome/welcome.component';
+import { RhythmPatternForms  } from './forms/step-1/step-1.component';
+import { MivesForms } from './forms/step-2/step-2.component';
+import { WeightsForms } from './forms/step-3/step-3.component';
 
 export const routes: Routes = [
     { path: '', component: WelcomePage },
+    { path: 'forms/rhythm', component: RhythmPatternForms },
+    { path: 'forms/mives', component: MivesForms },
+    { path: 'forms/weights', component: WeightsForms },
 ];
