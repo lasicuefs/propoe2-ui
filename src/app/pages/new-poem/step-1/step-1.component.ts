724e7c09430110bedb788e603022f97279a5677f
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-12-23 12:47:33 -0300

    remove unused import

--- src/app/pages/new-poem/step-1/step-1.component.ts
+++ src/app/pages/new-poem/step-1/step-1.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { PatternUnit } from './pattern-unit/pattern-unit.component';
 import { StanzaPattern } from './stanza-pattern/stanza-pattern.component';
 
 @Component({
   selector: 'app-step-1',
-  imports: [RouterLink, PatternUnit, StanzaPattern],
+  imports: [RouterLink, StanzaPattern],
   templateUrl: './step-1.component.html',
   styleUrl: './step-1.component.css'
 })
