7b8152576e25680bc9697f044e6f5036af45c74e
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-12-23 13:48:47 -0300

    add behavior to + button

--- src/app/pages/new-poem/step-1/step-1.component.ts
+++ src/app/pages/new-poem/step-1/step-1.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { StanzaPattern } from './stanza-pattern/stanza-pattern.component';
+import { Component } from '@angular/core'
+import { RouterLink } from '@angular/router'
+import { StanzaPattern } from './stanza-pattern/stanza-pattern.component'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-step-1',
-  imports: [RouterLink, StanzaPattern],
+  imports: [RouterLink, StanzaPattern, CommonModule],
   templateUrl: './step-1.component.html',
   styleUrl: './step-1.component.css'
 })
 export class RhythmPatternForms {
+  stanzas: number = 2
 
+  addStanza(): void {
+    this.stanzas++
+  }
+
+  stanzaArray(): number[] {
+    return Array(this.stanzas)
+      .fill(0)
+      .map((_, i) => i)
+  }
 }
