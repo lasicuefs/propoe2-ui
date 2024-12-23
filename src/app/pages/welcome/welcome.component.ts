671e1b7c65d4ad2c87daac6054fd6f0bc8d69d02
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-12-22 22:56:35 -0300

    replace elements by subtitle

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Title } from '../../shared/title/title.component';
+import { SubtitleComponent } from "../../shared/subtitle/subtitle.component";
 
 @Component({
   selector: 'app-welcome',
-  imports: [RouterLink, Title],
+  imports: [RouterLink, Title, SubtitleComponent],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
