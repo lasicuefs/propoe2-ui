0dfff572aad1664c1cb25bf23fd59f93b57d6b28
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-12-22 22:41:52 -0300

    replace h1 elements by title components

--- src/app/pages/new-poem/step-2/step-2.component.ts
+++ src/app/pages/new-poem/step-2/step-2.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Title } from "../../../shared/title/title.component";
 
 @Component({
   selector: 'app-step-2',
-  imports: [],
+  imports: [Title],
   templateUrl: './step-2.component.html',
   styleUrl: './step-2.component.css'
 })
--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Title } from '../../shared/title/title.component';
 
 @Component({
   selector: 'app-welcome',
-  imports: [RouterLink],
+  imports: [RouterLink, Title],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
