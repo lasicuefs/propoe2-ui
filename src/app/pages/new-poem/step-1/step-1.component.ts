267d161afd9f6bcec9a70b773c8d317dce16781a
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-12-22 22:42:05 -0300

    add a simple title to placeholder pages

--- src/app/pages/new-poem/step-1/step-1.component.ts
+++ src/app/pages/new-poem/step-1/step-1.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Title } from '../../../shared/title/title.component';
 
 @Component({
   selector: 'app-step-1',
-  imports: [],
+  imports: [Title],
   templateUrl: './step-1.component.html',
   styleUrl: './step-1.component.css'
 })
--- src/app/pages/new-poem/step-3/step-3.component.ts
+++ src/app/pages/new-poem/step-3/step-3.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Title } from "../../../shared/title/title.component";
 
 @Component({
   selector: 'app-step-3',
-  imports: [],
+  imports: [Title],
   templateUrl: './step-3.component.html',
   styleUrl: './step-3.component.css'
 })
