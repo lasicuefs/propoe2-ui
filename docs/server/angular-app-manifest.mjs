71254d05d432436b7b1c976ee9bb4ddaddaea8c1
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-12-23 19:14:57 -0300

    fix deployment

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -27,13 +27,13 @@ export default {
   }
 ],
   assets: {
-    'index.csr.html': {size: 2259, hash: 'b80ee1b3d14b3138ac4c12595e902272ff635666ef08355bc472d96ab216d807', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1121, hash: '37dd96b85755c0e84c7f3e09209f33a618b67ea630d346d3c82188f9a952d567', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'new/mives/index.html': {size: 9459, hash: '023c82244e729126b8b264946f01ce28a05050f53839e28e46aa997ec7b2fac0', text: () => import('./assets-chunks/new_mives_index_html.mjs').then(m => m.default)},
-    'new/weights/index.html': {size: 11484, hash: 'ae469c46331d3792fb7395bbfc12708aab8359e4c6ad3e638168c8818b541f42', text: () => import('./assets-chunks/new_weights_index_html.mjs').then(m => m.default)},
-    'new/rhythm/index.html': {size: 12588, hash: 'fe074897893e0d4c35639fd2e00ef333666c80e795aff7adb80391357145c7a3', text: () => import('./assets-chunks/new_rhythm_index_html.mjs').then(m => m.default)},
-    'poem/index.html': {size: 11198, hash: '309bcce08669d043cc138498d9163d1e842d4bc9717c5773ef07367041195f8b', text: () => import('./assets-chunks/poem_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 11217, hash: '0e9ea4720b88b3e1f44846e9fbcc573612c45ada5ee4db767603bc34907d0631', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 2323, hash: '8c2e187bc5e020c1075b4e418cb25fee9c5c8920bdb5cc10e00c3bf90bd45fce', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1169, hash: '7635b234aae7045456d4829a2d6abce43d546c729eda98d52a1c071b5291bfdb', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'new/rhythm/index.html': {size: 4368, hash: 'ec26790a46f6a5921fdf04b341ecfde7bb81cd6873546db982a54e4ade20c203', text: () => import('./assets-chunks/new_rhythm_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 2729, hash: 'ecd58727e0d19c007d563dbad28aeb150133c0270c4466ea6472dc12c93f5fd8', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'new/mives/index.html': {size: 1925, hash: '1363b136fb127068a612d43b2ac65ecf58f24a098a569fd321391aebd4838a41', text: () => import('./assets-chunks/new_mives_index_html.mjs').then(m => m.default)},
+    'poem/index.html': {size: 3831, hash: 'fd5a1b90d5d3818a7704c05f9f16df1a8877bd3bcd4d269ea89be419aec27241', text: () => import('./assets-chunks/poem_index_html.mjs').then(m => m.default)},
+    'new/weights/index.html': {size: 3235, hash: 'fdea3f228006555e4c3af215b49bb5eb559659482ac5bfc7dd4ab12fc669369b', text: () => import('./assets-chunks/new_weights_index_html.mjs').then(m => m.default)},
     'styles-KY7EDP7L.css': {size: 24777, hash: 'U9or4IKYhRE', text: () => import('./assets-chunks/styles-KY7EDP7L_css.mjs').then(m => m.default)}
   },
 };
