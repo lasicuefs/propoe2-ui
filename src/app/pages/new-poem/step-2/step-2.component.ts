dbfd97f2b08b9588f13d51beda23b3642b96b09f
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-01-17 19:21:14 -0300

    add file input and routes to the next step

--- src/app/pages/new-poem/step-2/step-2.component.ts
+++ src/app/pages/new-poem/step-2/step-2.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Steps } from "../steps/steps.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-step-2',
@@ -8,5 +9,14 @@ import { Steps } from "../steps/steps.component";
   styleUrl: './step-2.component.css'
 })
 export class MivesForms {
+  constructor(private router: Router) {}
+
+  receiveFile(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      // TODO: handle file to submit
+      this.router.navigate(['/new/weights']);
+    }
+  }
 
 }
