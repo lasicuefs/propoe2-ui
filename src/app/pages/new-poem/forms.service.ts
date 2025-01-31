a20295073e1141ee4026b3b29c769f72489ecc22
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-01-30 22:28:28 -0300

    format file

--- src/app/pages/new-poem/forms.service.ts
+++ src/app/pages/new-poem/forms.service.ts
@@ -1,32 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class Forms {
   private data: any = {
-    "prosody": {
-      "pattern": "",
-      "rhythm": []
+    'prosody': {
+      'pattern': '',
+      'rhythm': []
     },
-    "weights": {
-      "vocal-harmony": 1,
-      "accentuation": 1,
-      "tonic-position": 1,
-      "internal-rhyme": 1,
-      "rhythmic-structure": 1
+    'weights': {
+      'vocal-harmony'      : 1,
+      'accentuation'       : 1,
+      'tonic-position'     : 1,
+      'internal-rhyme'     : 1,
+      'rhythmic-structure' : 1
     }
   }
 
-  updatePatternAndRhythm(pattern: string, rhythm: number[]): void {
-    this.data.prosody.pattern = pattern;
-    this.data.prosody.rhythm = rhythm;
+  updatePatternAndRhythm(pattern: string, rhythm: number[]) {
+    this.data.prosody.pattern = pattern
+    this.data.prosody.rhythm = rhythm
     console.log(this)
   }
 
-  weightsFrom(weights: any): void {
-    this.data.weights = weights;
-    console.log(this);
+  weightsFrom(weights: any) {
+    this.data.weights = weights
+    console.log(this)
   }
 
 }
