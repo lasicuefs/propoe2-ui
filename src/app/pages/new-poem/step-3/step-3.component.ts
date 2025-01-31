bd16624ea5de5209697116f2d64a8b0b6cd02fe5
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-01-30 22:23:29 -0300

    format file

--- src/app/pages/new-poem/step-3/step-3.component.ts
+++ src/app/pages/new-poem/step-3/step-3.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { Router } from '@angular/router'
 
 // Import Local Components
-import { AttributeField } from './attribute-field/attribute-field.component';
-import { Steps } from "../steps/steps.component";
+import { AttributeField } from './attribute-field/attribute-field.component'
+import { Steps } from "../steps/steps.component"
 
 // Import Local Services
-import { Forms } from '../forms.service';
+import { Forms } from '../forms.service'
 
 @Component({
   selector: 'app-step-3',
@@ -18,27 +18,31 @@ import { Forms } from '../forms.service';
 export class WeightsForms {
 
   weights: { text: string, id: string, value: number }[] = [
-    { text: 'Rima toante e consoante', id: 'vocal-harmony', value: 1.0 },
-    { text: 'Acentuação', id: 'accentuation', value: 1.0 },
-    { text: 'Posição tônica', id: 'tonic-position', value: 1.0 },
-    { text: 'Rima interna', id: 'internal-rhyme', value: 1.0 },
-    { text: 'Estrutura rítimica', id: 'rhythmic-structure', value: 1.0 },
-  ];
+    { text: 'Rima toante e consoante', id: 'vocal-harmony',      value: 1.0 },
+    { text: 'Acentuação',              id: 'accentuation',       value: 1.0 },
+    { text: 'Posição tônica',          id: 'tonic-position',     value: 1.0 },
+    { text: 'Rima interna',            id: 'internal-rhyme',     value: 1.0 },
+    { text: 'Estrutura rítimica',      id: 'rhythmic-structure', value: 1.0 },
+  ]
 
-  constructor(private formsService: Forms, private router: Router) {}
+  constructor(private forms: Forms, private router: Router) {}
 
   updateFormsService(): void {
-    const weights: any = {};
-    this.weights.forEach(weight => {
-      const input = document.querySelector(`input[name="${weight.id}"]`) as HTMLInputElement;
-      weights[weight.id] = parseFloat(input.value);
-    });
-    this.formsService.updateWeights(weights);
+    const weights: any = {}
+
+    this.weights.forEach( weight => {
+      const $ = document.querySelector
+      
+      const input = $(`input[name="${weight.id}"]`) as HTMLInputElement;
+      weights[weight.id] = parseFloat(input.value)
+    })
+
+    this.forms.updateWeights(weights)
   }
 
-  onGenerate(event: Event): void {
-    event.preventDefault();
-    this.updateFormsService();
-    this.router.navigate(['/poem']);
+  onGenerate(event: Event) {
+    event.preventDefault()
+    this.updateFormsService()
+    this.router.navigate(['/poem'])
   }
 }
