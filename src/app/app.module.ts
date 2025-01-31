01567c449a74328b15ebcc8ec5f43222273a0bae
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-01-31 16:46:45 -0300

    remove comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-// ...existing code...
 
 @NgModule({
   declarations: [
-    // ...existing code...
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    // ...existing code...
   ],
   providers: [],
-  bootstrap: [/* Your main component */]
+  bootstrap: []
 })
 export class AppModule { }
