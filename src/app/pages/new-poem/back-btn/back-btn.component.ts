978610e9b86b5d402a6484d0fe3c1d3602ef4a91
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-02-10 11:37:23 -0300

    Create a simple back-button component

---
+++ src/app/pages/new-poem/back-btn/back-btn.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'back-btn',
+  imports: [CommonModule, RouterLink],
+  templateUrl: './back-btn.component.html',
+  styleUrl: './back-btn.component.css'
+})
+export class BackBtnComponent {
+  @Input() route: string = ""
+}
