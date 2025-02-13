f769e682302a4e9b80f46729fe224c01d9efeea1
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-02-13 16:13:47 -0300

    remove onChanges

--- src/app/pages/new-poem/step-3/attribute-field/attribute-field.component.ts
+++ src/app/pages/new-poem/step-3/attribute-field/attribute-field.component.ts
@@ -15,15 +15,4 @@ export class AttributeField {
 
     @Input()
     value: number = 1.0
-
-    ngOnChanges() {
-        if (!this.value) {
-            this.value = 1.0
-            return
-        }
-
-        if (this.value < 0 || this.value > 1) {
-            throw new Error("Value must be a float between 0 and 1")
-        }
-    }
 }
