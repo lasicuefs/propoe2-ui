7afc7f3db52bd29ad35bbaa76a8c10d30dd1c27a
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-03-15 12:55:13 -0300

    clear Feedback

--- src/app/pages/poem/Feedback.ts
+++ src/app/pages/poem/Feedback.ts
@@ -25,23 +25,24 @@ export class FeedbackService {
     template: `
         @let star = "⭐";
 
-        <section class="feedback z-50 fixed top-0 left-0 w-screen h-screen backdrop-blur-xl flex items-center justify-center">
-            <form id="feedback-form" class="bg-slate-100 p-6 rounded-xl shadow-xl flex flex-col gap-4">
+        <section class="z-50 fixed top-0 left-0 w-screen h-screen backdrop-blur-xl flex items-center justify-center">
+            <form class="bg-slate-100 p-6 rounded-xl shadow-xl flex flex-col gap-4">
+                
                 <!-- Rating Stars -->
-                <div class="rating flex flex-row-reverse justify-center gap-2 mb-4">
+                <div class="flex flex-row-reverse justify-center gap-2 mb-4">
                     @for (i of [5, 4, 3, 2, 1]; track $index ) {
                         <input type="radio" id="star-{{i}}" name="rating" value={{i}} class="hidden">
                         <label for="star-{{i}}" class="text-3xl cursor-pointer grayscale-[1]">{{star}}</label>
                     }
                 </div>
+
                 <!-- Optional Comment -->
-                <div class="comment">
-                    <textarea id="comment" name="comment" 
-                        placeholder="Comente sobre sua experiência..." value=""
-                        class="w-full p-2 bg-slate-100 border rounded outline-none resize-none"
-                        cols="45" rows="4"
-                        maxlength="180"></textarea>
-                </div>
+                <textarea id="comment" name="comment" 
+                    placeholder="Comente sobre sua experiência..." value=""
+                    class="w-full p-2 bg-slate-100 border rounded outline-none resize-none"
+                    cols="45" rows="4"
+                    maxlength="180"></textarea>
+                
                 <!-- Submit and Cancel buttons -->
                 <div class="flex gap-4">
                     <button type="button" (click)="onCancel($event)" class="action-btn w-full">
@@ -51,6 +52,7 @@ export class FeedbackService {
                         Enviar
                     </button>
                 </div>
+
             </form>
         </section>
     `,
