236f9f41351bbc27d91c3a52a4b814213ba172e9
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-03-15 11:37:18 -0300

    create common funnctions files

---
+++ src/app/pages/poem/common.ts
@@ -0,0 +1,20 @@
+export const post = async (route: string, body: any) => {
+    const PROPOE_API = "localhost:8000"
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            // For development purposes
+            'Ignore-Certificate-Errors': 'true'
+        },
+        body: JSON.stringify(body)
+    }
+
+    return fetch(`http://${PROPOE_API}/${route}`, options)
+}
+
+export const trace = (obj: any, msg: string = "") => {
+    console.log(msg, JSON.stringify(obj))
+    return obj
+}
