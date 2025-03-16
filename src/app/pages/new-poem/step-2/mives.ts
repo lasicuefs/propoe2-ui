98d2366f5194c1b248b25bd07d9d3b4fc7b93ba6
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-03-15 21:06:30 -0300

    inline Mives

--- src/app/pages/new-poem/step-2/mives.ts
+++ src/app/pages/new-poem/step-2/mives.ts
@@ -8,8 +8,23 @@ import { ReturnButton } from "../return-back/button"
 @Component({
     selector: "app-step-2",
     imports: [Steps, ReturnButton],
-    templateUrl: "./mives.html",
-    styleUrl: "./mives.css",
+    template: `
+        <main class="content-container">
+            <h1 class="title">Arquivo MIVES</h1>
+            <div class="flex gap-4 mt-4">
+                <button class="action-btn" (click)="next()">Amostra Padrão</button>
+            </div>
+
+            <!-- Bottom -->
+            <span class="hint-msg">
+                Ecolha um de nossos arquivos MIVES.
+            </span>
+
+            <!-- Floating Navigation -->
+            <return-back route="/new/rhythm" pageTitle="Rima & Rítimo"></return-back>
+            <forms-steps [currentStep]="2" [totalSteps]="3"></forms-steps>
+        </main>
+    `
 })
 export class MivesForms {
     private router = inject(Router)
