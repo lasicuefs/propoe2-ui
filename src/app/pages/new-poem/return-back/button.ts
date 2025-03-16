642dbfbe7131e5c859613a81d70803bc3d989f86
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-03-15 21:34:32 -0300

    inline button

--- src/app/pages/new-poem/return-back/button.ts
+++ src/app/pages/new-poem/return-back/button.ts
@@ -1,15 +1,46 @@
-import { Component, Input } from "@angular/core"
+import { Component, input } from "@angular/core"
 import { RouterLink } from "@angular/router"
 
 @Component({
     selector: "return-back",
     imports: [RouterLink],
-    templateUrl: "./button.html",
-    styleUrl: "./button.css",
+    template: `
+        <a
+            [routerLink]="route()"
+            class="fixed top-1/2 left-0 transform -translate-y-1/2 ml-8"
+            title="{{pageTitle()}}"
+        >
+            <button
+                class="bg-white hover:bg-gray-100 text-gray-800 rounded-full p-2 shadow-lg"
+            >
+                <!-- Left Arrow Chevron -->
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-chevron-double-left"
+                    viewBox="0 0 16 16"
+                >
+                    <path
+                        fill-rule="evenodd"
+                        d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"
+                    />
+                    <path
+                        fill-rule="evenodd"
+                        d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"
+                    />
+                </svg>
+            </button>
+        </a>
+    `
 })
 export class ReturnButton {
-    @Input()
-    route: string = ""
-    @Input()
-    pageTitle: string = ""
+    // Attributes
+    // ----------
+    // route sets the href based on our application's route
+    // pageTitle sets our title, that enables tooltip when hovering it
+
+    route = input<string>("")
+    pageTitle = input<string>("")
 }
